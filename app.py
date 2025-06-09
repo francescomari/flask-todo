3c30d0565609fb6d52d984b4777db400fea3a350
Francesco Mari <mari.francesco@gmail.com>
2025-06-09 23:21:46 +0200

    Format comments

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ todos = []
 
 app = Flask(__name__)
 
-# A template is identified by its path relative from the `templates`
-# directory. Usually, a template needs extra data to correctly render. In
-# this case, we pass the to-do list to the template. Inside the template,
-# the to-do list is accessible through the `todos` variable.  Flask handles
-# the HTTP GET request and returns the rendered HTML as the response.
+# A template is identified by its path relative from the `templates` directory.
+# Usually, a template needs extra data to correctly render. In this case, we
+# pass the to-do list to the template. Inside the template, the to-do list is
+# accessible through the `todos` variable.  Flask handles the HTTP GET request
+# and returns the rendered HTML as the response.
 
 
 @app.route("/")
@@ -22,35 +22,33 @@ def index():
     return render_template("index.html", todos=todos)
 
 
-# This route only responds to POST requests. Accessing this route from the
-# web browser, which generates a GET request to "http://localhost:5000/add",
-# will result in a 405 Method Not Allowed response.
+# This route only responds to POST requests. Accessing this route from the web
+# browser, which generates a GET request to "http://localhost:5000/add", will
+# result in a 405 Method Not Allowed response.
 #
-# The special variable `request`, imported from the `flask` module,
-# represents the current request processed by your application. This
-# variable contains all kinds of information about the request, including
-# the form data that a web `<form>` sends. The `request.form` is a
-# dictionary that maps the form fields to their corresponding values.
+# The special variable `request`, imported from the `flask` module, represents
+# the current request processed by your application. This variable contains all
+# kinds of information about the request, including the form data that a web
+# `<form>` sends. The `request.form` is a dictionary that maps the form fields
+# to their corresponding values.
 #
-# This function performs just a minimal validation of its input: if the
-# to-do item is empty, nothing is added to the list. In more complex
-# applications, validation on the server-side is very important and usually
-# a great source of bugs. Always pay attention to how you validate user
-# input on the server!
+# This function performs just a minimal validation of its input: if the to-do
+# item is empty, nothing is added to the list. In more complex applications,
+# validation on the server-side is very important and usually a great source of
+# bugs. Always pay attention to how you validate user input on the server!
 #
-# Finally, whether we add a to-do item to the list or not, this handler
-# responds with a 302 Found response, commonly known as a "redirect". When
-# the browser receives a 302 response, it makes another HTTP request to the
-# path provided in the Location header. In this example, we are using the
-# url_for() function to retrieve the path of the handler for the `"index"`
-# function. This mechanism allows us to change the path associated to a
-# handler without rewriting every part of the application that hardcodes
-# that path.
+# Finally, whether we add a to-do item to the list or not, this handler responds
+# with a 302 Found response, commonly known as a "redirect". When the browser
+# receives a 302 response, it makes another HTTP request to the path provided in
+# the Location header. In this example, we are using the url_for() function to
+# retrieve the path of the handler for the `"index"` function. This mechanism
+# allows us to change the path associated to a handler without rewriting every
+# part of the application that hardcodes that path.
 #
 # We use a redirect after handling the POST request (the post/redirect/get
-# pattern). This prevents duplicate submissions if the user refreshes the
-# page, because the browser will perform a GET request to the main page
-# instead of resubmitting the form data. See more about this pattern:
+# pattern). This prevents duplicate submissions if the user refreshes the page,
+# because the browser will perform a GET request to the main page instead of
+# resubmitting the form data. See more about this pattern:
 #
 # https://en.wikipedia.org/wiki/Post/Redirect/Get
 
@@ -64,20 +62,20 @@ def add():
 
 
 # This is another handler that only responds to POST requests. This time,
-# though, the handler is responsible for marking a to-do item as "done". In
-# this application, this means removing the to-do item from the list.
+# though, the handler is responsible for marking a to-do item as "done". In this
+# application, this means removing the to-do item from the list.
 #
 # The index of the to-do item is passed as part of the request path. Every
-# string in a path in the form `<index>` is used as a wildcard to match
-# multiple requests with similar paths. In this example, this handler is
-# invoked when the request path is `/done/0`, `/done/99`, etc. Flask gives
-# the value of the placeholder `<index>` to the parameter with the same
-# name. For more information, see:
+# string in a path in the form `<index>` is used as a wildcard to match multiple
+# requests with similar paths. In this example, this handler is invoked when the
+# request path is `/done/0`, `/done/99`, etc. Flask gives the value of the
+# placeholder `<index>` to the parameter with the same name. For more
+# information, see:
 #
 # https://flask.palletsprojects.com/en/stable/quickstart/#variable-rules
 #
-# After validating the index and maybe removing a to-do item from the list,
-# we issue a redirect to the index page of the application.
+# After validating the index and maybe removing a to-do item from the list, we
+# issue a redirect to the index page of the application.
 
 
 @app.route("/done/<int:index>", methods=["POST"])
